9184ca092e8e959b4ee5d3a259b099a43721206d
Parthasarathy G <83574852+parthasarathy27@users.noreply.github.com>
2024-04-06 17:20:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,55 +1,42 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const userRouter = require('./routes/userRouter');
-const noteRouter = require('./routes/noteRouter');
-const path = require('path');
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const userRouter = require('./routes/userRouter')
+const noteRouter = require('./routes/noteRouter')
+const path = require('path')
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+const app = express()
+app.use(express.json())
+app.use(cors())
 
-// Routes
-app.use('/users', userRouter);
-app.use('/api/notes', noteRouter);
+//routes
+
+app.use('/users', userRouter)
+app.use('/api/notes', noteRouter)
+
 
 // Connect to MongoDB
 const URL = process.env.MONGODB_URL;
 
-mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(URL)
   .then(() => {
     console.log('Connected to MongoDB');
   })
   .catch((err) => {
     console.error('Error connecting to MongoDB:', err);
-    process.exit(1); // Exit the process if unable to connect to MongoDB
   });
-
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static(path.join(__dirname, 'client/build')));
-
-  // Serve index.html for any other routes
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  
+// Below MongoDB and  Above Listen Server
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) =>{
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
   });
 }
 
-// Handle 404 errors
-app.use((req, res, next) => {
-  res.status(404).send("Sorry, can't find that!");
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
-// Listen server
-const PORT = process.env.PORT || 5000;
+// listen Server
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-  console.log('Server is running on port', PORT);
-});
+    console.log('Server is running on port', PORT)
+})
